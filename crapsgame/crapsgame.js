d199feb3a5df7a52dc20b5fd64ccefe7b9c2f9dd
09Godslove <200godslove@gmail.com>
2025-10-23 15:33:30 +0100

    regex to validate username

--- crapsgame/crapsgame.js
+++ crapsgame/crapsgame.js
@@ -3,11 +3,18 @@ const usernameInput = 'username-input'
 const resistrationPane = 'craps-game-username-input'
 const mainSection = 'crapsgame-main-section'
 
-
+//get and validate username
 function registerCrapsPlayer (){
         let username = document.getElementById(usernameInput).value
-        removeRegistration()
-        showMainsection()
+        let firstCharIsDigit = /^[0-9]|[^a-zA-Z0-9_]/g
+        if (username.length < 5 || firstCharIsDigit.test(username)) {
+            alert('Username has to be alphanumeric, greater than 5 and does not contain special characters')
+        }else{
+            removeRegistration()
+            showMainsection()
+        }
+
+
 }
 function removeRegistration (){
     document.getElementById(resistrationPane).style.display = 'none'
