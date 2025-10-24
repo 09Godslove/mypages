30feda80d2c9f1280a381ad3fbff040716a78799
09Godslove <200godslove@gmail.com>
2025-10-24 15:22:27 +0100

    Added stats section

--- crapsgame/crapsgame.js
+++ crapsgame/crapsgame.js
@@ -1,17 +1,27 @@
+//Main Data
+let username = ''
+
+//Game stats
+let beginMoney = 1000
+let BeginRounds = 0
 //HTML elements ID
 const usernameInput = 'username-input'
 const resistrationPane = 'craps-game-username-input'
 const mainSection = 'crapsgame-main-section'
+const crapsUsername = 'craps-user'
+const startMoney = 'craps-money'
+const startRound = 'craps-rounds'
 
 //get and validate username
 function registerCrapsPlayer (){
-        let username = document.getElementById(usernameInput).value
+        username = document.getElementById(usernameInput).value
         let firstCharIsDigit = /^[0-9]|[^a-zA-Z0-9_]/g
         if (username.length < 5 || firstCharIsDigit.test(username)) {
             alert('Username has to be alphanumeric, greater than 5 and does not contain special characters')
         }else{
             removeRegistration()
             showMainsection()
+            DisplayData()
         }
 
 
@@ -21,4 +31,15 @@ function removeRegistration (){
 }
 function showMainsection(){
     document.getElementById(mainSection).style.display = 'block'
+}
+function DisplayData (){
+    document.getElementById(crapsUsername).innerHTML = username
+    DisplayMoney(beginMoney)
+    DisplayRounds(BeginRounds)
+}
+function DisplayMoney (money){
+    document.getElementById(startMoney).innerHTML = money
+}
+function DisplayRounds (round){
+    document.getElementById(startRound).innerHTML = round
 }
