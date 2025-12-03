8d74f7bbf2cd1d4fcce1068dc30245663029dc1c
09Godslove <200godslove@gmail.com>
2025-12-03 15:04:28 +0100

    Completed random Quote generator

--- randomQuoteGenerator/randomQuoteGenerator.js
+++ randomQuoteGenerator/randomQuoteGenerator.js
@@ -1,4 +1,31 @@
+const randomQuoteGeneratorID = document.getElementById('random-quote-generator')
+const randomQuoteTextID = document.getElementById('random-quote-text')
+const randomQuoteAuthorID = document.getElementById('random-quote-author')
+
+const colors = [
+    ['#AB274F','#7C0902'],
+    ['#FE6F5E','#BF4F51'],
+    ['#660000','#FB607F'],
+    ['#FFD700','#EDC9AF'],
+    ['#FF7F50','#FF5800'],
+    ['#AB4E52','#BC8F8F'],
+    ['#A91101','#44362F'],
+    ['#882D17','#55872f'],
+    ['#FACA16','#FFFACD'],
+    ['#12FaF0','#004225'],
+    ['#66FF00','#FF9966']
+
+]
+
+function getRandomColor(arr){
+    colorArr = arr[Math.floor(Math.random() * arr.length)]
+    return colorArr[Math.floor(Math.random() * colorArr.length)]
+}
+
 function getQuote(){
+    let c1 = getRandomColor(colors)
+    let c2 = getRandomColor(colors)
+    randomQuoteGeneratorID.style.background = `linear-gradient(45deg, ${c1}, ${c2})`
     fetch('https://api.quotable.io/quotes/random')
     .then(response => {
         if(!response.ok){
@@ -9,8 +36,8 @@ function getQuote(){
     .then(data => {
         const quoteText = data[0].content
         const quoteAuthor = data[0].author
-        document.getElementById('random-quote-text').innerHTML = quoteText
-        document.getElementById('random-quote-author').innerHTML = quoteAuthor
+        randomQuoteTextID.innerHTML = quoteText
+        randomQuoteAuthorID.innerHTML = quoteAuthor
     })
     .catch(error =>{
         // Handle Error here
