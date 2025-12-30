8a47eeb846dd70e34e09702c9bf92e0b4e3cae22
09Godslove <200godslove@gmail.com>
2025-12-30 20:00:48 +0100

    Implemented conditional rendering on stock data dashboard

--- personal-website/src/stockAnalysisDashboard/stockAnalysisDashboard.tsx
+++ personal-website/src/stockAnalysisDashboard/stockAnalysisDashboard.tsx
@@ -1,26 +1,71 @@
 import { useState } from 'react'
 import { analyzeStock } from "./stockAnalysisDashboard"
+import { Oval } from 'react-loader-spinner'
 
 function StockAnalysisDashboard() {
   const [finalData, setFinaldata] = useState<any>(null)
-  const [stockSymbol, setStockSymbol] = useState<any>(null)
+  const [stockSymbol, setStockSymbol] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+  const [gotData, setGotData] = useState(false)
 
-  async function runStockAnalysis() {
-    const stockData = await analyzeStock(stockSymbol)
-    setFinaldata(stockData)
+  function goBack(){
+    setIsLoading(false)
+    setGotData(false)
   }
 
+  async function runStockAnalysis() {
+    setIsLoading(true)
+    const stockData = await analyzeStock(stockSymbol)
+    if(stockData){
+      setFinaldata(stockData)
+      setGotData(true)
+      setIsLoading(false)
+    }else{
+      goBack()
+    }
+
+  }
+
+  if(gotData){
+    return(
+      <>
+        <div onClick={() => goBack()}>Back</div>
+        <div>
+          <div>{JSON.stringify(finalData)}</div>
+        </div>
+      </>
+    )
+  }
   return (
     <>
         <div>
             <div id="stock-analysis-dashboard-title">stock Analysis Dashboard</div>
-            <div id="stock-analysis-dashboard-title">Enter the symbol of the stock you wish to analyze. E.g MSFT.</div>
-            <input 
-              value={stockSymbol}
-              onChange={e => setStockSymbol(e.target.value)}
-            ></input>
-            <button className="stock-analysis-btn" onClick={() => runStockAnalysis()}>Analyze</button>
-            <div>{JSON.stringify(finalData)}</div>
+            {isLoading ? (
+              <div>
+                <Oval
+                height={80}
+                width={80}
+                color="#4fa94d"
+                visible={true}
+                ariaLabel="oval-loading"
+                secondaryColor="#4fa94d"
+                strokeWidth={2}
+                strokeWidthSecondary={2}
+            />
+              </div>
+            ) : (
+              <div>
+                 <div id="stock-analysis-dashboard-title">Enter the symbol of the stock you wish to analyze. E.g MSFT.</div>
+            
+                <input 
+                  value={stockSymbol}
+                  onChange={e => setStockSymbol(e.target.value)}
+                ></input>
+                <button className="stock-analysis-btn" onClick={() => runStockAnalysis()}>Analyze</button>
+              </div>
+            )}
+           
+            
         </div>
     </>
   )
